febcf908c8a94ffe15f29e59dfedf492845e504c
mGoodwolf <143968101+mGoodwolf@users.noreply.github.com>
2023-09-04 08:26:16 -0700

    Update web-demo.js

--- web-demo.js
+++ web-demo.js
@@ -20,4 +20,14 @@ function main() {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
+  // Vertex shader program
+const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
 }
