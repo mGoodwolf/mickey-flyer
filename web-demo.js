93c1a77001a5f53f2a6862f5c9f895bc8a7f7e35
mGoodwolf <143968101+mGoodwolf@users.noreply.github.com>
2023-09-04 08:26:54 -0700

    Update web-demo.js

--- web-demo.js
+++ web-demo.js
@@ -29,5 +29,10 @@ const vsSource = `
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
   `;
+const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
 
 }
