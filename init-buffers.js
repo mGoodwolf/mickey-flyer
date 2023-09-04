5ea5ef09f5da493f61dabc88bb8f5c8481f3330d
mGoodwolf <143968101+mGoodwolf@users.noreply.github.com>
2023-09-04 08:46:07 -0700

    Update init-buffers.js

--- init-buffers.js
+++ init-buffers.js
@@ -4,6 +4,13 @@ function initBuffers(gl) {
   return {
     position: positionBuffer,
   };
+  const colorBuffer = initColorBuffer(gl);
+
+return {
+  position: positionBuffer,
+  color: colorBuffer,
+};
+
 }
 
 function initPositionBuffer(gl) {
@@ -24,5 +31,32 @@ function initPositionBuffer(gl) {
 
   return positionBuffer;
 }
+function initColorBuffer(gl) {
+  const colors = [
+    1.0,
+    1.0,
+    1.0,
+    1.0, // white
+    1.0,
+    0.0,
+    0.0,
+    1.0, // red
+    0.0,
+    1.0,
+    0.0,
+    1.0, // green
+    0.0,
+    0.0,
+    1.0,
+    1.0, // blue
+  ];
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+  return colorBuffer;
+}
+
 
 export { initBuffers };
